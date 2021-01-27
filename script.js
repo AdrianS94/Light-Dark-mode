6a21a9ab87d7390add077aa21d66b6c29ff0e728
Adrian Staniste <adrian@192-168-0-144.rdsnet.ro>
2021-01-27 20:08:06 +0200

    update
    update

---
+++ script.js
@@ -0,0 +1,65 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box'); 
+
+//Dark or Light for images
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+//dark mode style 
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode ('dark');
+}
+
+//light mode style
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+//Switch theme 
+function switchTheme(event) {
+    if(event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+}
+
+//Event listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+//Check Local storage For theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
+
+
+//Animation
+gsap.from('nav', {duration: 1.3, scale: 0.5});
+gsap.from('div.theme-switch-wrapper', {duration: 1.3, opacity: 0, scale: 0.5, delay: 1});
+gsap.from('div.title-group', {duration: 1.5, opacity: 0, rotation: 360, scale: 0.5, delay: 1.5}); 
